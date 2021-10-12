52673b76a374fe454630d97f92f4bff1b09c101e
Mark Taylor <taylor26@seattleu.edu>
2021-10-11 23:56:54 -0700

    got rid of comments made from ICE

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-
+//change-mark
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
