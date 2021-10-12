d5749c45517ac23344b35ffa858b899f1283abeb
yuzhong <yzhong2@seattleu.edu>
2021-10-11 19:34:52 -0700

    practice merge conflict

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
-
+//merge-conflict
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
