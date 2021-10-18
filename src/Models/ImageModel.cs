d5ab6ec8c613b10f012e82523c6e6eb174292589
Mark Taylor <taylor26@seattleu.edu>
2021-10-17 20:47:21 -0700

    In Models folder create an image model that will be used to handle image upload by user and push to a SQL database.

--- src/Models/ImageModel.cs
+++ src/Models/ImageModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 
@@ -9,6 +11,13 @@ namespace ContosoCrafts.WebSite.Models
 {
     public class ImageModel
     {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int ImageID { get; set; }
 
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
     }
 }
