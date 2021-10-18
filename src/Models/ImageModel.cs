86270ce7d5e0de9cfaf2d6f6c2d329fbb40a95a0
Mark Taylor <taylor26@seattleu.edu>
2021-10-17 20:43:00 -0700

    Added new dependencies to project. EntityFrameWorkCore, .SqlServer, .Tools
    Added these dependencies to upload/retrieve images users add to our website.

---
+++ src/Models/ImageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class ImageModel
+    {
+
+    }
+}
