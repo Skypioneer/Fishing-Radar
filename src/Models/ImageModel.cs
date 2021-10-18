9dca61447ab7ff31356221f1f1dc9458e408853e
Mark Taylor <taylor26@seattleu.edu>
2021-10-17 20:55:26 -0700

    Added comments to describe aspects of ImageModel

--- src/Models/ImageModel.cs
+++ src/Models/ImageModel.cs
@@ -12,11 +12,16 @@ namespace ContosoCrafts.WebSite.Models
     public class ImageModel
     {
         [System.ComponentModel.DataAnnotations.Key]
+
+        /*ID for image uploaded*/
         public int ImageID { get; set; }
+
 
         [Column(TypeName = "nvarchar(50)")]
         public string Title { get; set; }
 
+
+        /*Unique Name for uploaded image*/
         [Column(TypeName = "nvarchar(100)")]
         public string ImageName { get; set; }
     }
