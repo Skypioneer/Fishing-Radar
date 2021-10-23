512f155a647012326acc57187e0757cd2f902638
Francis Kogge <koggefa@gmail.com>
2021-10-23 00:05:15 -0700

    Added TestSetup region code in Read.cshtml.Tests.cs.

--- UnitTests/Pages/Posts/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Posts/Read.cshtml.Tests.cs
@@ -1,6 +1,22 @@
-﻿namespace UnitTests.Pages.Posts.Read
+﻿using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+
+namespace UnitTests.Pages.Posts.Read
 {
     public class Read
     {
+        #region TestSetup
+        public static ReadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ReadModel(PageTestsHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
     }
 }
