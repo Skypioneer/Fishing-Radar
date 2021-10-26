4827a19979005c0c1a4596025fa8cce1f358a065
Mark Taylor <taylor26@seattleu.edu>
2021-10-26 16:01:20 -0700

    Created boilerplate unit test for MyPosts.cshtml.cs as a new file called MyPosts.cshtml.Test.cs.
    Currently, the page has no functionality and doesn't need code coverage but
    it will in the near future.

---
+++ UnitTests/Pages/Posts/MyPosts.cshtml.Tests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
+
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Pages.Posts;
+
+
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Services;
+
+
+
+namespace UnitTests.Pages.Posts.MyPosts
+{
+    public class MyPostsTest
+    {
+
+        #region TestSetup
+        public static MyPostsModel pageModel;
+
+        /// <summary>
+        /// Initialize a MyPostsModel for testing
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new MyPostsModel(PageTestsHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+    }
+}
