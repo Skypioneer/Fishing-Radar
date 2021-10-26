0878335456ee1448a98f22446a6b3917f514342e
williamzhxb3 <yzhong2@seattleu.edu>
2021-10-26 09:33:18 -0700

    Added comments for the Error Model class in Error.cshtml.cs file

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Logging;
 //change that im going to stash- mark
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Error model class to handle error page
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
@@ -15,11 +18,18 @@ namespace ContosoCrafts.WebSite.Pages
 
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Default Constructor with a logger for logging the error happenend
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Sets the requestID from the current activity ID
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
