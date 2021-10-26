5e7c9a4fd0dacfd1216994587c5d94553e5d0d74
Skypioneer <cwang@seattleu.edu>
2021-10-26 12:29:05 -0700

    Added a UT file for the Create page called Create.cshtml.Tests.cs in UniteTests/Pages/Posts

---
+++ UnitTests/Pages/Posts/Create.cshtml.Tests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+
+
+namespace UnitTests.Pages.Product.Create
+{
+    public class CreateTests
+    {
+        #region TestSetup
+        public static CreateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new CreateModel(PageTestsHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+            var oldCount = PageTestsHelper.ProductService.GetAllData().Count();
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(oldCount + 1, PageTestsHelper.ProductService.GetAllData().Count());
+        }
+        #endregion OnGet
+    }
+}
