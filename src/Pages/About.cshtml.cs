e58aba268d19a8a224cc1d2bd513bbc4365b4b73
williamzhxb3 <yzhong2@seattleu.edu>
2021-10-26 09:57:12 -0700

    Added comments for the AboutModel class in About.cshtml.cs

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -3,15 +3,24 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// AboutModel class to handle about page 
+    /// </summary>
     public class AboutModel : PageModel
     {
         private readonly ILogger<AboutModel> _logger;
 
+        /// <summary>
+        /// Default Constructor with a logger for logging info
+        /// </summary>
         public AboutModel(ILogger<AboutModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        ///  Currently empty because no need of it yet
+        /// </summary>
         public void OnGet()
         {
         }
