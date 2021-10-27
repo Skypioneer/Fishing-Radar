1f238b353a82a8a94e1e7b999443a0f89f8de932
Francis Kogge <koggefa@gmail.com>
2021-10-26 19:35:29 -0700

    Added Unit test file for the About page called About.cshtml.Tests.cs.

---
+++ UnitTests/Pages/About.cshtml.Tests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
+
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Pages.Posts;
+
+
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Services;
+
+
+
+namespace UnitTests.Pages
+{
+    public class AboutTest
+    {
+
+        #region TestSetup
+        public static AboutModel pageModel;
+
+        /// <summary>
+        /// Initialize a AboutModel for testing
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<AboutModel>>();
+
+            pageModel = new AboutModel(MockLoggerDirect)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Request_Should_Return_True()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+
+    }
+}
