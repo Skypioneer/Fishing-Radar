ebab8bd1ee8356ad6c7060b3927a366e3c23a19b
Skypioneer <cwang@seattleu.edu>
2021-10-26 19:14:34 -0700

    Added a UT file for the Profile page called Profile.cshtml.Tests.cs in UniteTests/Pages

---
+++ UnitTests/Pages/Profile.cshtml.Tests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
+
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Pages.Posts;
+
+
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Services;
+
+
+
+namespace UnitTests.Pages.Posts
+{
+    public class ProfileTest
+    {
+
+        #region TestSetup
+        public static ProfileModel pageModel;
+
+        /// <summary>
+        /// Initialize a ProfileModel for testing
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new ProfileModel()
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Request_Should_Return_True()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+
+    }
+}
