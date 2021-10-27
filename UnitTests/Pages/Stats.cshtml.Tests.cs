196590bac497f00675b5bf565edba6d13b41de5f
Silvuurleaf <taylor26@seattleu.edu>
2021-10-26 19:45:22 -0700

    Added boilerplate unit test for the Statspage in the file Stas.cshtml.Tests.cs.

---
+++ UnitTests/Pages/Stats.cshtml.Tests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Pages
+{
+    class StatsTests
+    {
+
+        #region TestSetup
+        public static StatsModel pageModel;
+
+        /// <summary>
+        /// Initialize a Stats model for testing
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            //Empty constructor for stats model
+            pageModel = new StatsModel()
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Request_Should_Return_True()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+
+        #endregion OnGet
+
+    }
+}
