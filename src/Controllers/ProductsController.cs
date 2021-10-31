0dacd0373982e3800e92b30336af71517057d90d
Francis Kogge <koggefa@gmail.com>
2021-10-31 14:12:37 -0700

    Added comments to ProductsController.cs.

--- src/Controllers/ProductsController.cs
+++ src/Controllers/ProductsController.cs
@@ -7,23 +7,43 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+    /// <summary>
+    /// Controller responsible for interaction with the Products.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
+        /// <summary>
+        /// Initializes the JsonFileProductService field.
+        /// </summary>
+        /// <param name="productService">A JsonFileProductService instance</param>
         public ProductsController(JsonFileProductService productService)
         {
             ProductService = productService;
         }
 
+        // Field for getting and performing actions on the product list
         public JsonFileProductService ProductService { get; }
 
+        /// <summary>
+        /// Http Get request
+        /// Gets the collection of products.
+        /// </summary>
+        /// <returns>Enumerable collection of products</returns>
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetProducts();
         }
 
+        /// <summary>
+        /// Http Patch request
+        /// Adds a rating to the specified product. It finds the given product from
+        /// the rating request.
+        /// </summary>
+        /// <param name="request">A RatingRequest instance</param>
+        /// <returns>OkResult, indicating successful request</returns>
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
@@ -32,9 +52,14 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Helper class for finding a product from its ProductID and Rating
+        /// </summary>
         public class RatingRequest
         {
+            // The product's ProductID
             public string ProductId { get; set; }
+            // The product's Rating
             public int Rating { get; set; }
         }
     }
