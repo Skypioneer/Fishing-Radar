e5a76d4c2b066d0b81d290233b1a597c1711dff2
williamzhxb3 <yzhong2@seattleu.edu>
2021-11-01 12:53:44 -0700

    added code comments to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -3,13 +3,25 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// A class that Build up the web host for the website
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// main function which can take in some parameters as configuration to create the host  
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// create the host with default configurations to start up the web
+        /// </summary>
+        /// <param name="args"> configurations that you can pass in</param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
