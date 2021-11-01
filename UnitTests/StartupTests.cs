f494687240817b1b21398f35aa3c132decb2d11c
williamzhxb3 <yzhong2@seattleu.edu>
2021-11-01 14:09:16 -0700

    removed duplicated test and added code comments to startupTests.cs

--- UnitTests/StartupTests.cs
+++ UnitTests/StartupTests.cs
@@ -5,15 +5,22 @@ using NUnit.Framework;
 
 namespace UnitTests.Pages.Startup
 {
+    /// <summary>
+    /// A test class for the Startup class
+    /// </summary>
     public class StartupTests
     {
         #region TestSetup
 
+        //initialization for the tests
         [SetUp]
         public void TestInitialize()
         {
         }
 
+        /// <summary>
+        /// creating a inside class with the base configuration of the exmaple web startup class
+        /// </summary>
         public class Startup : ContosoCrafts.WebSite.Startup
         {
             public Startup(IConfiguration config) : base(config) { }
@@ -21,22 +28,16 @@ namespace UnitTests.Pages.Startup
         #endregion TestSetup
 
         #region ConfigureServices
+        /// <summary>
+        /// test startup build function with valid default configuration
+        /// the webhost should be not null after build
+        /// </summary>
         [Test]
-        public void Startup_ConfigureServices_Valid_Defaut_Should_Pass()
+        public void Startup_ConfigureServices_Valid_Default_Should_Pass()
         {
             var webHost = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
             Assert.IsNotNull(webHost);
         }
         #endregion ConfigureServices
-
-        #region Configure
-        [Test]
-        public void Startup_Configure_Valid_Defaut_Should_Pass()
-        {
-            var webHost = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
-            Assert.IsNotNull(webHost);
-        }
-
-        #endregion Configure
     }
 }
