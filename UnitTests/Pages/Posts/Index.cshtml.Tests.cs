c254e0fcdc22a66cfc64c30bcdc586de0ad38fe0
skypioneer <cwang@seattleu.edu>
2021-11-02 00:29:08 -0700

    added comments for class definition to Index.cshtml.Tests.cs

--- UnitTests/Pages/Posts/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Posts/Index.cshtml.Tests.cs
@@ -8,6 +8,9 @@ using ContosoCrafts.WebSite.Pages.Posts;
 
 namespace UnitTests.Pages.Product.Index
 {
+    /// <summary>
+    /// Testing the IndexModel class for the index function of CRUDI
+    /// </summary>
     public class IndexTests
     {
         #region TestSetup
@@ -15,6 +18,9 @@ namespace UnitTests.Pages.Product.Index
 
         public static IndexModel pageModel;
 
+        /// <summary>
+        /// Initialize a IndexModel for testing
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -25,6 +31,10 @@ namespace UnitTests.Pages.Product.Index
 
         #endregion TestSetup
 
+        /// <summary>
+        /// Testing OnGet function with a valid Product ID
+        /// The model state should then be valid and the title should be matched accordingly
+        /// </summary>
         #region OnGet
         [Test]
         public void OnGet_Valid_Should_Return_Products()
