51a0934c0ed8a572876af174a7310dd09595417b
Francis Kogge <koggefa@gmail.com>
2021-11-02 10:58:37 -0700

    Added TestSetup region code to initialize test field in ProductModelTests.cs.

--- UnitTests/Models/ProductModelTests.cs
+++ UnitTests/Models/ProductModelTests.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
+    /// <summary>
+    /// Unit test class for ProductModel.cs.
+    /// </summary>
     class ProductModelTests
     {
+        #region TestSetup
+        // ProductModel instance to test on
+        public static ProductModel productModel;
+
+        /// <summary>
+        /// Initialize the ProductModel for testing
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            productModel = PageTestsHelper.ProductService.GetAllData().First();
+            
+        }
+        #endregion TestSetup
     }
 }
