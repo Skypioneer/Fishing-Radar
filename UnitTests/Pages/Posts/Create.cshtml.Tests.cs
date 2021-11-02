c1b30b34585758f290a196b12dd824c1edb929d2
skypioneer <cwang@seattleu.edu>
2021-11-02 00:40:48 -0700

    added comments for class definition in Create.cshtml.Tests.cs

--- UnitTests/Pages/Posts/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Posts/Create.cshtml.Tests.cs
@@ -7,11 +7,17 @@ using ContosoCrafts.WebSite.Pages.Product;
 
 namespace UnitTests.Pages.Product.Create
 {
+    /// <summary>
+    /// Testing the CreateModel class for the create function of CRUDI.
+    /// </summary>
     public class CreateTests
     {
         #region TestSetup
         public static CreateModel pageModel;
 
+        /// <summary>
+        /// Initialize a UpdateModel for testing
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -22,6 +28,10 @@ namespace UnitTests.Pages.Product.Create
 
         #endregion TestSetup
 
+        /// <summary>
+        /// Testing OnGet function with a valid Product ID
+        /// The model state should then be valid and the title should be matched accordingly
+        /// </summary>
         #region OnGet
         [Test]
         public void OnGet_Valid_Should_Return_Products()
