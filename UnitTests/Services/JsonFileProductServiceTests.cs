7d7557e2e67b1866bac3a39298558cf0a3b8be8d
Francis Kogge <koggefa@gmail.com>
2021-11-06 21:15:40 -0700

    Added AddRating unit test method for testing null ratings called AddRating_Valid_Null_Ratings_Should_Initialize_Ratings_Array in JsonFileProductServiceTests.cs.

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ContosoCrafts.WebSite.Models;
 using NUnit.Framework;
 
 namespace UnitTests.Services
@@ -24,6 +24,24 @@ namespace UnitTests.Services
         }
 
         #endregion TestSetup
+
+        [Test]
+        public void AddRating_Valid_Null_Ratings_Should_Initialize_Ratings_Array()
+        {
+            // Arrange
+            var data = new ProductModel()
+            {
+                Id = "new ID"
+            };
+            PageTestsHelper.ProductService.CreateData(data);
+
+            // Act
+            PageTestsHelper.ProductService.AddRating(data.Id, 5);
+            var result = PageTestsHelper.ProductService.GetAllData().First(x => x.Id == data.Id);
+
+            // Assert
+            Assert.AreEqual(new int[] { 5 }, result.Ratings);
+        }
 
     }
 }
