5ed078f98f2c8203c85c1ec7fe8663abdb70dfc8
Francis Kogge <koggefa@gmail.com>
2021-11-06 20:54:32 -0700

    Added TestSetup region code in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NUnit.Framework;
+
 namespace UnitTests.Services
 {
+    /// <summary>
+    /// Unit Test class for JsonFileProductService.cs
+    /// </summary>
     class JsonFileProductServiceTests
     {
+        #region TestSetup
+
+        /// <summary>
+        /// Default constructor to initialize tests.
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
     }
 }
