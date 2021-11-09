b4b034dd7cfcdce76fe4cc8072bca350e1fe6f27
williamzhxb3 <yzhong2@seattleu.edu>
2021-11-09 09:20:57 -0800

    added Unit tests for Program.cs

---
+++ UnitTests/ProgramTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using ContosoCrafts.WebSite;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// unit test class for program class
+    /// </summary>
+    class ProgramTests
+    {
+        #region TestSetup
+
+        //initialization for the tests
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+        #endregion TestSetup
+
+        #region ConfigureServices
+        /// <summary>
+        /// test startup build function with valid default configuration
+        /// the webhost should be not null after build
+        /// </summary>
+        [Test]
+        public void Program_CreateHostBuilder_Valid_Default_Should_Pass()
+        {
+            string[] config = new string[0];
+            //action
+            var webHost = Program.CreateHostBuilder(config);
+
+            Assert.IsNotNull(webHost);
+
+        }
+        #endregion ConfigureServices
+    }
+}
