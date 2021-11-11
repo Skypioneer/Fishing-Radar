9d44d2b710827dd9d3aa877b826c5ead24580bc6
Francis Kogge <koggefa@gmail.com>
2021-11-10 21:28:17 -0800

    Added new model class for fishing spot data called FishingSpotModel.

---
+++ src/Models/FishingSpotModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Fishing spot model class to represent fishing spot data.
+    /// </summary>
+    public class FishingSpotModel
+    {
+    }
+}
