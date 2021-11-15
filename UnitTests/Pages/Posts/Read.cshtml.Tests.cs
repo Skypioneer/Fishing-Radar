54e6b584672cc31c3e0ec01c720b0b33bd9e9dc8
Francis Kogge <koggefa@gmail.com>
2021-11-15 13:12:00 -0800

    Added unit test method for invalid ID input called OnGet_Invalid_Should_Redirect_To_Index_Page in Read.cshtml.Tests.cs.

--- UnitTests/Pages/Posts/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Posts/Read.cshtml.Tests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 
 using ContosoCrafts.WebSite.Pages.Product;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Pages.Posts.Read
 {
@@ -20,6 +21,11 @@ namespace UnitTests.Pages.Posts.Read
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
+        /// OnGet test method
+        /// Product attribute should be initialized when Get request is made which is verified
+        /// by checking the title of the first product.
+        /// </summary>
         [Test]
         public void OnGet_Valid_Should_Return_Products()
         {
@@ -31,6 +37,23 @@ namespace UnitTests.Pages.Posts.Read
             // Assert
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
             Assert.AreEqual("How to handle shark if you catch them", pageModel.Product.Title);
+        }
+
+        /// <summary>
+        /// OnGet test method
+        /// Ppassing an invalid ID should redirect Read page to the index page
+        /// so that the website doesn't crash
+        /// </summary>
+        [Test]
+        public void OnGet_Invalid_Should_Redirect_To_Index_Page()
+        {
+            // Arrange
+
+            // Act
+            var result = pageModel.OnGet("ID that doesn't exist") as RedirectToPageResult;
+
+            // Assert
+            Assert.AreEqual("./Index", result.PageName);
         }
         #endregion OnGet
     }
