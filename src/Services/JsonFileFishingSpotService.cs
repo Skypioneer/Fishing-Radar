bf079b771c111bfac5c6403d7a8af759680f9d75
Francis Kogge <koggefa@gmail.com>
2021-11-14 19:15:42 -0800

    Added new file for processing of fishing spot JSON database called JsonFileFishingSpotService.cs.

---
+++ src/Services/JsonFileFishingSpotService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    /// <summary>
+    /// JSON file processing class for the fishing spot data JSON file.
+    /// </summary>
+    public class JsonFileFishingSpotService
+    {
+    }
+}
