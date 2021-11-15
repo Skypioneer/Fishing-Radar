204b6df1a3fb67c54ef5ebca376afe613886e7e6
Francis Kogge <koggefa@gmail.com>
2021-11-14 21:24:36 -0800

    Added new UnitTest file and test initialization code in FishingSpotModelTests.cs.

---
+++ UnitTests/Models/FishingSpotModelTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    class FishingSpotModelTests
+    {
+        #region TestSetup
+        // FishingSpotModel instance to test on
+        public static FishingSpotModel fishingSpotModel;
+
+        /// <summary>
+        /// Initialize the FishingSpotModel for testing
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            fishingSpotModel = PageTestsHelper.FishingSpotService.GetAllData().First();
+
+        }
+        #endregion TestSetup
+    }
+}
