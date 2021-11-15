db159ae0ca891fed079dad1a4b3f09a962b5673f
Francis Kogge <koggefa@gmail.com>
2021-11-14 21:25:29 -0800

    Added unit test method to test FishingSpotModel attributes called ToString_Valid_Should_Return_Serialized_Json_String in FishingSpotModelTests.cs.

--- UnitTests/Models/FishingSpotModelTests.cs
+++ UnitTests/Models/FishingSpotModelTests.cs
@@ -24,5 +24,29 @@ namespace UnitTests.Models
 
         }
         #endregion TestSetup
+
+        /// <summary>
+        /// ToString should return a serialized Json string. Checks if the FishingSpotModel
+        /// instance's ToString matches the test data instance's ToString after manually
+        /// setting each field.
+        /// </summary>
+        [Test]
+        public void ToString_Valid_Should_Return_Serialized_Json_String()
+        {
+            // Arrange
+            var data = new FishingSpotModel();
+            {
+                data.Id = fishingSpotModel.Id;
+                data.SpotName = fishingSpotModel.SpotName;
+                data.City = fishingSpotModel.City;
+                data.Url = fishingSpotModel.Url;
+            }
+
+            // Act
+            var result = data.ToString();
+
+            // Assert
+            Assert.AreEqual(fishingSpotModel.ToString(), result);
+        }
     }
 }
