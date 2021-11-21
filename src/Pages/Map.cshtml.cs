079289217c2b3b0df078b0a01d6d6b996a70b121
Francis Kogge <koggefa@gmail.com>
2021-11-21 12:28:20 -0800

    Added REST Post functionality so users can create and upload to fishingSpot Json database in Map.cshtml.cs.

--- src/Pages/Map.cshtml.cs
+++ src/Pages/Map.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
@@ -10,6 +11,9 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class MapModel : PageModel
     {
+        // JSON file processing helper
+        public JsonFileProductService<FishingSpotModel> FishingSpotService { get; }
+
         /// <summary>
         /// Initializes the fishing spot service attribute.
         /// </summary>
@@ -19,19 +23,56 @@ namespace ContosoCrafts.WebSite.Pages
             FishingSpotService = fishingSpotService;
         }
 
-        // JSON file processing helper
-        public JsonFileProductService<FishingSpotModel> FishingSpotService { get; }
+        // Fishing spot object for data creation
+        [BindProperty]
+        public FishingSpotModel FishingSpot { get; set; }
 
         // Enumerable list of fishing spots from the JSON database
-        public IEnumerable<FishingSpotModel> FishingSpots { get; private set; }
+        public IEnumerable<FishingSpotModel> FishingSpotList { get; private set; }
 
         /// <summary>
         /// REST Get request
-        /// Retrieve all the fishing spots to be displayed on the web page.
+        /// Retrieve all the fishing spots to be displayed on the web page and creates
+        /// a default FishingSpot instance.
         /// </summary>
         public void OnGet()
         {
-            FishingSpots = FishingSpotService.GetAllData();
+            FishingSpotList = FishingSpotService.GetAllData();
+            FishingSpot = CreateData();
+        }
+
+        /// <summary>
+        /// REST post request
+        /// Save the newly created data to the JSON dataset via the FishingSpotService.
+        /// </summary>
+        /// <returns>Redirection to the same page</returns>
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            // Create and save the data to the JSON dataset
+            FishingSpotService.CreateData(FishingSpot);
+
+            // After saving data, redirect to same page to reload the locations list
+            return RedirectToPage();
+        }
+
+        /// <summary>
+        /// Creates a new FishingSpotModel that can be added to the database.
+        /// </summary>
+        /// <returns>FishingSpotModel instance with default values</returns>
+        private FishingSpotModel CreateData()
+        {
+            return new FishingSpotModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                SpotName = "Enter Spot Name",
+                City = "Enter City",
+                Url = "Enter URL"
+            };
         }
     }
 }
