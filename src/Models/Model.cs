0862961805eed858ce1e3fab6f3d6b8a080e2ca0
Francis Kogge <koggefa@gmail.com>
2021-11-23 14:13:35 -0800

    Added comments to Model.cs.

--- src/Models/Model.cs
+++ src/Models/Model.cs
@@ -5,8 +5,16 @@ using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Interface for Model classes.
+    /// </summary>
     public interface Model
     {
+        /// <summary>
+        /// Returns the json file name that stores data for the 
+        /// specific Model class.
+        /// </summary>
+        /// <returns>File name</returns>
         public string GetFileName();
     }
 }
