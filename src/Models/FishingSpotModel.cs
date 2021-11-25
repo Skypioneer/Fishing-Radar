b87b74be01d839272e19e6c22140121d0623b253
Francis Kogge <koggefa@gmail.com>
2021-11-25 15:17:05 -0800

    Added input validation annotations bove attributes in FishingSpotModel.cs.

--- src/Models/FishingSpotModel.cs
+++ src/Models/FishingSpotModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -15,12 +16,18 @@ namespace ContosoCrafts.WebSite.Models
         // Get/Set for the fishing spot ID
         public string Id { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 85, MinimumLength = 1, ErrorMessage = "The Fishing Spot Name field must be between {2} and {1} characters long.")]
         // Get/Set for the name of the fishing spot
         public string SpotName { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 85, MinimumLength = 1, ErrorMessage = "The City field must be between {2} and {1} characters long.")]
         // Get/Set for the city in which the fishing spot is located
         public string City { get; set; }
 
+        [Required]
+        [Url]
         // Get/Set for a URL link to a website with information about the fishing spot
         public string Url { get; set; }
 
